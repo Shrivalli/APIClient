c588eac773c75359baee84022563f10132db2819
Shrivalli Maheshwaran <shrivalli04@gmail.com>
2021-08-10 14:13:23 +0530

    first commit

---
+++ APIClient/Models/Emp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APIClient.Models
+{
+    public class Emp
+    {
+        public int Eid { get; set; }
+        public string Ename { get; set; }
+        public double? sal { get; set; }
+        public DateTime? doj { get; set; }
+        public string city { get; set; }
+        public string branch { get; set; }
+
+        
+    }
+}
+
+
